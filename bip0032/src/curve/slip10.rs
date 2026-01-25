c8fb81ea3c3833b8bf82db238106c9e46ab3b63e
Qinxuan Chen <koushiro.cqx@gmail.com>
2026-01-25 22:23:31 +0800

    chore(bip0032): update docs and benchmarks (#121)

--- bip0032/src/curve/slip10.rs
+++ bip0032/src/curve/slip10.rs
@@ -1,12 +1,12 @@
-//! SLIP-10 curve marker traits.
+//! SLIP-0010 curve marker traits.
 
 use super::Curve;
 
-/// Marker trait for SLIP-10 compatible curves.
+/// Marker trait for SLIP-0010 curves.
 pub trait Slip10Curve: Curve {}
 
-/// Marker trait for SLIP-10 curves that only allow hardened derivation.
+/// Marker trait for SLIP-0010 curves that only allow hardened derivation.
 pub trait Slip10HardenedOnlyCurve: Slip10Curve {}
 
-/// Marker trait for SLIP-10 curves that allow non-hardened derivation.
+/// Marker trait for SLIP-0010 curves that allow non-hardened derivation.
 pub trait Slip10NonHardenedCurve: Slip10Curve {}
